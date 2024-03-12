c1a422a35bd5143476e4aebe9d26a0c221eb5657
Alexey <124546102+Alexey-Perepelkin@users.noreply.github.com>
2024-03-12 21:14:59 +0300

    hw_1

---
+++ hw1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+    printf("Hello world!\n");
+    printf("\n");
+    printf("Let’s\n");
+    printf("\tgo\n");
+    printf("\t\tto walk\n");
+    printf("\n");
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+    printf("\n");
+    printf("  __        __\n");
+    printf("/    \\ __ /    \\\n");
+    printf("\\ __ /    \\ __ /\n");
+    printf("/    \\ __ /    \\\n");
+    printf("\\ __ /    \\ __ /\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    return 0;
+}
